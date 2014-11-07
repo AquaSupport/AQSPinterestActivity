fbda0229eacda1b46d3221a031c404ae63c79c75
Kai INUI <lied.der.optik@gmail.com>
2014-11-08 07:42:01 +0900

    gitignore

--- Pods/Target Support Files/Pods-AQSPinterestActivityTests-OCMock/Pods-AQSPinterestActivityTests-OCMock-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AQSPinterestActivityTests_OCMock : NSObject
-@end
-@implementation PodsDummy_Pods_AQSPinterestActivityTests_OCMock
-@end
--- Pods/Target Support Files/Pods-AQSPinterestActivityTests/Pods-AQSPinterestActivityTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AQSPinterestActivityTests : NSObject
-@end
-@implementation PodsDummy_Pods_AQSPinterestActivityTests
-@end
